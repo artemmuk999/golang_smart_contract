bd3d6afa4f462ada1525629628a676bad4e31483
huytai-dev27 <huytai.star27@gmail.com>
2025-01-03 17:25:24 +0900

    add review

--- cmd/server/entrypoint.sh
+++ cmd/server/entrypoint.sh
@@ -17,3 +17,5 @@ migrate -database "${APP_DSN}" -path ./migrations up
 
 echo "[`date`] Starting server..."
 ./server -config ${CONFIG_FILE} >> /var/log/app/server.log 2>&1
+
+//////////////////////review///////////
